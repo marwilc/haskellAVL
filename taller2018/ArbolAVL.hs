c47a510bece48b81fa527917bd406e5f24070876
Marwil Campos <marwilcampos@gmail.com>
2018-02-11 22:31:14 -0400

    implementada la funcion crearAVL

--- taller2018/ArbolAVL.hs
+++ taller2018/ArbolAVL.hs
@@ -3,7 +3,7 @@
 -- 11/02/2018 -}
 
 {- Modulo arbol AVL mas sus funciones predeterminadas-}
-module ArbolAVL(ArbolAVL(Nulo, Nodo), nulo, esNulo{-, crearAVL, raiz, izq, der, numNodos, esHoja, insertarAVL, eliminarAVL, buscarAVL, alturaAVL, balancear-})
+module ArbolAVL(ArbolAVL(Nulo, Nodo), nulo, esNulo, crearAVL{-, raiz, izq, der, numNodos, esHoja, insertarAVL, eliminarAVL, buscarAVL, alturaAVL, balancear-})
 where -- en el modulo se especifican dos estructuras una esructura Nulo (Arbol Nulo) y un Nodo con dos sub arboles.
 data ArbolAVL t = Nulo | Nodo t (ArbolAVL t) (ArbolAVL t) deriving (Show)
 
@@ -15,3 +15,11 @@ nulo Nulo = Nulo
 esNulo::ArbolAVL t -> Bool
 esNulo Nulo = True
 esNulo _ = False
+
+{- Funcion que crea un arbolAVl-}
+crearAVL::ArbolAVL t -> t -> ArbolAVL t
+crearAVL Nulo root = Nodo root Nulo Nulo
+crearAVL (Nodo root l r) e
+          | root == e = Nodo root l r
+          | root  < e = Nodo root l crearAVL(r e)
+          | root  > e = Nodo root crearAVL(l e) r
